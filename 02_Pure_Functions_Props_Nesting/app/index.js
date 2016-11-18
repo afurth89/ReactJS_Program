fd749c2c744dd477e95260e9ebabf92623f7e753
Andrew Furth <andrew.furth@gmail.com>
2016-11-18 11:01:57 -0800

    module complete

--- 02_Pure_Functions_Props_Nesting/app/index.js
+++ 02_Pure_Functions_Props_Nesting/app/index.js
@@ -1,15 +1,70 @@
+var USER_DATA = {
+  name: 'Andrew Furth',
+  username: 'afurth89',
+  image: 'https://avatars3.githubusercontent.com/u/12887095?v=3&s=460'
+}
+
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-var HelloWorld = React.createClass({
+// React render method
+// fn(data) = View
+
+/*
+  Focused
+  Independent
+  Reusable
+  Small
+  Testable
+*/
+
+var ProfileLink = React.createClass({
   render: function () {
     return (
-      <div>Hello Andrew</div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
     )
   }
-})
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var ProfilePic = React.createClass({
+  render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}></img>
+  }
+});
+
+var Avatar = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image}/>
+        <ProfileName name={this.props.user.name}/>
+        <ProfileLink username={this.props.user.username}/>
+      </div>
+    )
+  }
+});
+
+
+
+
+
+
+
+
 
 ReactDOM.render(
-  <HelloWorld/>,
+  <Avatar user={USER_DATA}/>,
   document.getElementById('app')
 )
