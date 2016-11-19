06f65093df3ff65953490c8aa0a798ad6a478d5a
Andrew Furth <andrew.furth@gmail.com>
2016-11-18 17:22:04 -0800

    module complete

--- 06_thisKeyword_Axios_Promises_GithubAPI/app/components/ConfirmBattle.js
+++ 06_thisKeyword_Axios_Promises_GithubAPI/app/components/ConfirmBattle.js
@@ -1,10 +1,15 @@
 var React = require('react')
 
+// PUKE TRICK: places props onto screen to easily check if they're correct
+function puke(obj) {
+  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
   return (
     props.isLoading
       ? <p>LOADING!</p>
-      : <p>CONFIRM BATTLE!</p>
+      : <div>CONFIRM BATTLE!: {puke(props)}</div>
   )
 }
 
--- 06_thisKeyword_Axios_Promises_GithubAPI/app/containers/ConfirmBattleContainer.js
+++ 06_thisKeyword_Axios_Promises_GithubAPI/app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react')
 var ConfirmBattle = require('../components/ConfirmBattle')
+var githubHelpers = require('../utils/githubHelpers')
 
 // 'Container' components are usually responsible for:
   // STATE and 
@@ -16,7 +17,7 @@ var ConfirmBattleContainer = React.createClass({
 
   // Very FIRST function to run if a route is called where the component is present
   getInitialState() {
-    console.log("getInitialState")
+    // console.log("getInitialState")
     return {
       isLoading: true,
       playersInfo: []
@@ -25,25 +26,32 @@ var ConfirmBattleContainer = React.createClass({
 
   // Runs BEFORE this component renders to the view
   componentWillMount() {
-    console.log("componentWillMount")
+    // console.log("componentWillMount")
   },
   
   // Runs AFTER this component renders to the view
   componentDidMount() {
-    console.log("componentDidMount")
+    // console.log("componentDidMount")
     var query = this.props.location.query
     // Fetch info from github, the update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))  // 'this' now refers to the outer function (componentDidMount's function)
   },
 
   // Once component is mounted, if there are props to receive
   componentWillReceiveProps: function(nextProps) {
-    console.log("componentWillReceiveProps")
+    // console.log("componentWillReceiveProps")
     
   },
 
   // If you navigate away from that route where the component is present
   componentWillUnmount: function() {
-    console.log("componentWillUnmount")
+    // console.log("componentWillUnmount")
     
   },
 
